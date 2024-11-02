4894570d9346f900b58a2bc198df812ffe7c0226
Tammineni Monika <141040099+MonikaTammineni@users.noreply.github.com>
2024-11-02 10:49:21 +0530

    Add files via upload
    added files

---
+++ auth.js
@@ -0,0 +1,18 @@
+// server/middleware/auth.js
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+  if (!token) {
+    return res.status(401).json({ success: false, message: 'Access Denied' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ success: false, message: 'Invalid Token' });
+  }
+};
+
+module.exports = auth;
